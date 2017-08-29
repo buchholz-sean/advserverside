2e267bd7ea4163d673880c573fddc7f7bb9adc24
Sean Buchholz <smbuchholz@fullsail.edu>
2017-08-28 22:29:54 -0400

    - Updated routing to route everything through index.js - Added Bootstrap for easy styling - Implemented dynamic navigation using Pug templating

--- app1/app1/routes/index.js
+++ app1/app1/routes/index.js
@@ -3,7 +3,24 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+      title: 'Home',
+      navitems: [
+          { link: '/', content: 'Home' },
+          { link: '/users', content: 'Users' }
+      ]
+   });
+});
+
+router.get('/users', function(req, res, next) {
+  res.render('users', {
+      title: 'Users',
+      users: [ { "name": "Sean", "lastName": "Buchholz" } ],
+      navitems: [
+          { link: '/', content: 'Home' },
+          { link: '/users', content: 'Users' }
+      ]
+   });
 });
 
 module.exports = router;
