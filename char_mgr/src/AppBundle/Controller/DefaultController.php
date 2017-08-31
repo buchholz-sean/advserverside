004a8ad1ff5b343f78701a4b8f2fe72cdc50e5d6
Sean Buchholz <smbuchholz@fullsail.edu>
2017-08-31 11:11:23 -0400

    Implemented basic routing & templating for Character Manager project

--- char_mgr/src/AppBundle/Controller/DefaultController.php
+++ char_mgr/src/AppBundle/Controller/DefaultController.php
@@ -13,9 +13,19 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $charsList = array(
+            "Aldara" => "Wizard",
+            "Arder" => "Immolator",
+            "Astrafel" => "Bard",
+            "Cecil" => "Barbarian",
+            "Dru" => "Druid",
+            "Erroll" => "Thief",
+            "Niskadora" => "Ranger"
+        );
+
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
-        ]);
+        return $this->render('default/index.html.twig', array(
+            'charsList' => $charsList
+        ));
     }
 }
