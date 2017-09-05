809e237b26f17cf214cf9b01b20fb403cbd9be4e
Sean Buchholz <smbuchholz@fullsail.edu>
2017-09-05 16:39:26 -0400

    Form management per Symfony Forms lecture on FSO

--- char_mgr/src/AppBundle/Controller/DefaultController.php
+++ char_mgr/src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,21 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Validator\Constraint;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\NotNull;
+use Symfony\Component\Validator\Constraints\Email as EmailConstraint;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Validator\Constraints\File;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
 class DefaultController extends Controller
 {
     /**
@@ -23,9 +38,51 @@ class DefaultController extends Controller
             "Niskadora" => "Ranger"
         );
 
-        // replace this example code with whatever you need
         return $this->render('default/index.html.twig', array(
             'charsList' => $charsList
         ));
+    }
+
+    /**
+     * @Route("/registerForm", name="registerForm")
+     */
+    public function registerForm(Request $req)
+    {
+        $form = $this->createFormBuilder(null)
+        ->setAction($this->generateUrl("registerForm"))
+        ->add("name", TextType::class, array("required"=>true,"constraints"=>[
+            new NotBlank(array("message"=>"This field cannot be blank"))
+            ]))
+        ->add("email", TextType::class, array("required"=>true,"constraints"=>[
+            new EmailConstraint(array("message"=>"This field must be a valid email address")),
+            new NotBlank(array("message"=>"This field cannot be blank"))
+            ]))
+        ->add("myFile", FileType::class, array("constraints"=>[
+            new File(
+                array(
+                "maxSize"=>"2M",
+                "mimeTypes" => [
+                    'application/pdf',
+                    'application/x-pdf'
+                    ],
+                "mimeTypesMessage" => "Please upload a valid PDF")
+            )
+            ]))
+        ->add("save", SubmitType::class)
+        ->getForm();
+
+        $form->handleRequest($req);
+
+        if ($req->isMethod("POST")) {
+            if ($form->isValid()) {
+                $file = $form->get("myFile")->getData();
+                $fileName = md5(uniqid()).".".$file->guessExtension();
+                $file->move("/Users/SeanBuchholz/Desktop/proj", $fileName);
+                
+                return $this->render("form/submitted.html.twig", array("title"=>"Register"));
+            }
+        };
+
+        return $this->render("form/form.html.twig", array("title"=>"Register", "form"=>$form->createView()));
     }
 }
