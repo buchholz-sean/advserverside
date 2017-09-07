ab9e2f93116a6b1ec4ca55292cdea2ae7a39103d
Sean Buchholz <smbuchholz@fullsail.edu>
2017-09-07 16:39:17 -0400

    - Redirected routing to implement ‘Users’ route - Created standard signup form view - Whitespace changes throughout

--- app1/app1/routes/index.js
+++ app1/app1/routes/index.js
@@ -3,24 +3,22 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-      title: 'Home',
-      navitems: [
-          { link: '/', content: 'Home' },
-          { link: '/users', content: 'Users' }
-      ]
-   });
+    res.render('index', {
+        title: 'Home',
+        navitems: [{
+                link: '/',
+                content: 'Home'
+            },
+            {
+                link: '/users',
+                content: 'Users'
+            }
+        ]
+    });
 });
 
-router.get('/users', function(req, res, next) {
-  res.render('users', {
-      title: 'Users',
-      users: [ { "name": "Sean", "lastName": "Buchholz" } ],
-      navitems: [
-          { link: '/', content: 'Home' },
-          { link: '/users', content: 'Users' }
-      ]
-   });
-});
+// router.get('/registration', function(req, res, next){
+//
+// })
 
 module.exports = router;
--- app1/app1/routes/users.js
+++ app1/app1/routes/users.js
@@ -3,7 +3,53 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+    res.render('form', {
+        title: 'Sign Up',
+        navitems: [{
+            link: '/',
+            content: 'Home'
+        }, {
+            link: '/users',
+            content: 'Users'
+        }]
+    });
 });
+
+router.post('/', function(req, res, next) {
+    var email = req.body.email;
+    var pass = req.body.password;
+    var passVerify = req.body.passVerify;
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regex.test(email)) {
+        res.render('form', {
+            title: 'Sign Up',
+            msg: 'Please enter a valid email address',
+            navitems: [{
+                link: '/',
+                content: 'Home'
+            }, {
+                link: '/users',
+                content: 'Users'
+            }]
+        })
+    } else {
+        if (pass != passVerify) {
+            res.render('form', {
+                title: 'Sign Up',
+                msg: 'Passwords do not match',
+                navitems: [{
+                    link: '/',
+                    content: 'Home'
+                }, {
+                    link: '/users',
+                    content: 'Users'
+                }]
+            });
+        } else {
+            res.send("Thank you for signing up");
+        }
+    }
+
+})
 
 module.exports = router;
